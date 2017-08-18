06a923d94670df5e59fd3b54606d706009d63872
mobei12 <505453856@qq.com>
2017-08-18 09:57:57 +0800

    提交

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
+// error handler啊啊啊
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
