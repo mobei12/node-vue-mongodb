5a4889f321b1c1b45cfd5549651df8609b845f24
mobei12 <505453856@qq.com>
2017-08-22 11:14:07 +0800

    添加jade模块，

--- routes/appInterface.js
+++ routes/appInterface.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 const crypto = require('crypto');//加密模块
-
 const mongodbUtil = require('../utils/mongodbUtil');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var query = {};
-    console.log(JSON.stringify(req));
+    const mobile = req.query.mobile;
+    const psw = req.query.psw;
+    const hash = crypto.createHash('md5');
+    hash.update(psw);
+    var pwdMd5 = hash.digest('hex');
+    const query = {phone:mobile};
+    mongodbUtil.findOneByQuery(query,'user',[],function(result){
+      const resultData = {};
+      if(pwdMd5 !== result.pwd){
+        resultData.isSuccess = false;
+        resultData.Msg = "密码错误";
+        res.send(resultData);
+      }else{
+        result.isSuccess = true;
+        res.send(result);
+      }
+    })
+    
   });
 module.exports = router;
